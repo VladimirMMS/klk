9eff966b7ea705f22274a20ef2c7a8bc91470cbe
CINCINNATUS <vladimirmercado2001@gmail.com>
2020-03-10 15:31:24 -0400

    lkl

---
+++ players.py
@@ -0,0 +1,74 @@
+# from pro import total
+# import random 
+# class Players:
+#     def __init__(self, name, deck_of_player):
+#         self.name = name
+#         self.deck_of_player = deck_of_player
+
+#     def deck_of_table(self, deck_table):
+#         deck_table = []
+        
+
+#     def Creation_of_player(self, player):
+#         score = 0
+#         while score == 500:
+#             random.shuffle(total)
+#             for i in range(7):
+#                 card1 = total.pop(0)
+#                 self.player_deck.append(card1)
+#             card_table = total.pop(0)
+#             self.deck_of_table.append(card_table)
+#             if self.deck_of_table == 'change_of_color' or self.deck_of_table == 'change_of_color+4':
+#                 take = (input('Introduce el nombre de la carta que quitar sacar:'))
+#                 for walk in self.deck_of_player:
+#                     if walk in take:
+#                         self.deck_of_player.remove(take)
+#                         self.deck_table.append(take)
+#                 else:
+#                     if deck_of_player[0][walk] == deck_table[0]:
+                    
+                    
+                    # self.deck_of_player in self.deck_of_table[0]:
+                    # self.deck_of_player.remove(deck_of_table[0])
+
+# player = ['carta1']
+total = ['carta1', 'carta1', 'carta3','carta4', 'carta5', 'carta6', 'carta7']
+b = total.pop(len(total[0:-1]))
+print(b)
+
+
+# for i in total:
+#     if i in player:
+#         print('si')
+
+
+
+
+
+# print(b)
+
+
+# for i in range(7):
+#     card1 = total.pop(0)
+#     player.append(card1)
+# print(player)
+    
+
+
+
+
+
+
+
+
+
+
+# player1 = Players()
+# player1.esa()
+
+
+
+
+
+
+
---
+++ pro.py
@@ -0,0 +1,107 @@
+class Deck:
+    def __init__(self,color,number,ability, ability_well):
+        self.color = color
+        self.number = number
+        self.ability = ability
+        self.ability_well = ability_well
+
+
+
+class Table:
+    def __init__(self):
+        self.decks = [] 
+    
+    def creation_of_card(self):
+        colors = ['Blue', 'Red', 'Yellow','Green']
+        colors_argu = []
+        number_argu = []
+        for color_0 in colors:
+            for n in range(0,10):
+                colors_argu.append(color_0)
+                number_argu.append(n)
+                self.decks.append([color_0, n])
+        for color_1 in colors:
+            for n in range(1,10):
+                colors_argu.append(color_1)
+                number_argu.append(n)
+                self.decks.append([color_1, n])
+        return [colors_argu,number_argu]
+        
+    def creation_of_special_deck(self):
+        colors_spe = ['Blue', 'Red', 'Yellow','Green']
+        abilities = ['Cancelation','Direction', 'sum_tow']
+        ability_special = []
+        for color in colors_spe:
+            for ability in abilities:
+                ability_special.append([color,ability])
+                self.decks.append([color,ability])
+        for color in colors_spe:
+            for ability in abilities:
+                ability_special.append([color,ability])
+                self.decks.append([color,ability])
+        return ability_special
+    def creation_of_willcard(self):
+        ability_willcard = ['change_of_color', 'change_of_color+4','change_of_color', 'change_of_color+4','change_of_color', 
+                                'change_of_color+4','change_of_color', 'change_of_color+4']
+        ability_will = []
+        for ability_w in ability_willcard:
+            ability_will.append([ability_w])
+            self.decks.append([ability_w])
+        return ability_will
+    
+jugador = Table()
+num_color = jugador.creation_of_card()
+creation = jugador.creation_of_special_deck()
+will = jugador.creation_of_willcard()
+total = jugador.decks
+
+
+
+
+
+
+
+
+
+
+
+
+# class Deck_special:
+#     def __init__(self,ability,color):
+#         self.color = color
+#         self.ability = ability
+
+# class Table_s:
+#     deck_spe = []
+
+
+
+    
+
+
+# mesa_s = Table_s()
+# mesa_s.creation_of_special_deck()
+# for deck_s in mesa_s.deck_spe:
+#     print(deck_s.ability, deck_s.color)
+    
+# class Deck_willcard:
+#     def __init__(self,ability):
+#         self.ability = ability
+
+# class Table_w:
+#     def __init__(self):
+#         self.deck_will = []
+    
+
+
+# mesa_w = Table_w()
+# mesa_w.creation_of_willcard()
+# for deck_w in mesa_w.deck_will:
+#     print(deck_w.ability)
+
+
+
+
+
+
+
